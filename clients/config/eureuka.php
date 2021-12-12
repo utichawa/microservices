997598ec217298f57d5210d44874cd863c597d45
Yassine Mechiche <mechiche.yassine@gmail.com>
2021-12-12 12:40:04 +0100

    Add the docker file to clients microservice

--- clients/config/eureuka.php
+++ clients/config/eureuka.php
@@ -2,13 +2,13 @@
 
 return [
     'server' => [
-        'eurekaDefaultUrl' => 'http://localhost:8761/eureka',
+        'eurekaDefaultUrl' => 'http://discovery:8761/eureka/',
         'hostName' => 'clients_microservice',
         'appName' => 'Client',
         'ip' => '127.0.0.1',
         'port' => ['80', true],
-        'homePageUrl' => 'http://localhost:80',
-        'statusPageUrl' => 'http://localhost:80/info',
-        'healthCheckUrl' => 'http://localhost:80/health'
+        // 'homePageUrl' => 'http://discovery:80/eureka/',
+        // 'statusPageUrl' => 'http://discovery:80/eureka',
+        // 'healthCheckUrl' => 'http://discovery:80/health'
     ]
 ];
