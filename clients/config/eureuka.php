f165246c84cabe800aba157b532af9d2c0608d89
Yassine Mechiche <mechiche.yassine@gmail.com>
2021-12-12 16:45:17 +0100

    Update project

--- clients/config/eureuka.php
+++ clients/config/eureuka.php
@@ -2,13 +2,13 @@
 
 return [
     'server' => [
-        'eurekaDefaultUrl' => 'http://discovery:8761/eureka/',
-        'hostName' => 'clients_microservice',
-        'appName' => 'Client',
-        'ip' => '127.0.0.1',
-        'port' => ['80', true],
-        // 'homePageUrl' => 'http://discovery:80/eureka/',
-        // 'statusPageUrl' => 'http://discovery:80/eureka',
-        // 'healthCheckUrl' => 'http://discovery:80/health'
+        'eurekaDefaultUrl' => 'http://localhost:8761/eureka/',
+        'hostName' => 'localhost',
+        'appName' => 'client-s',
+        'ip' => '192.168.127.1',
+        'port' => ['8763', true],
+        'homePageUrl' => 'http://192.168.127.1:8763/client-s/',
+        'statusPageUrl' => 'http://192.168.127.1:8763/client-s',
+        'healthCheckUrl' => 'http://192.168.127.1:8763/health'
     ]
 ];
