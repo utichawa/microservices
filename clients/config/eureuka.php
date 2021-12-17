81e43c5224d453e330638f914264086bd6f74f4e
Yassine Mechiche <mechiche.yassine@gmail.com>
2021-12-17 15:26:09 +0100

    Update the discovery server and client configuration APIs

--- clients/config/eureuka.php
+++ clients/config/eureuka.php
@@ -2,13 +2,13 @@
 
 return [
     'server' => [
-        'eurekaDefaultUrl' => 'http://localhost:8761/eureka/',
-        'hostName' => 'localhost',
-        'appName' => 'client-s',
-        'ip' => '192.168.127.1',
-        'port' => ['8763', true],
-        'homePageUrl' => 'http://192.168.127.1:8763/client-s/',
-        'statusPageUrl' => 'http://192.168.127.1:8763/client-s',
-        'healthCheckUrl' => 'http://192.168.127.1:8763/health'
+        'eurekaDefaultUrl' => 'http://discovery:8761/eureka/',
+        'hostName' => 'client-service',
+        'appName' => 'client-service',
+        'ip' => 'discovery',
+        'port' => ['8000', true],
+        'homePageUrl' => 'http://discovery:8000',
+        'statusPageUrl' => 'http://discovery:8000',
+        'healthCheckUrl' => 'http://discovery:8000/health'
     ]
 ];
