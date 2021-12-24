82c7370bb1cdcabedda5d468a3617b74ce0e0649
Yassine Mechiche <mechiche.yassine@gmail.com>
2021-12-24 20:57:18 +0100

    add config eureka

--- clients/config/database.php
+++ clients/config/database.php
@@ -81,7 +81,7 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'discovery'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
--- clients/config/eureuka.php
+++ clients/config/eureuka.php
@@ -2,13 +2,13 @@
 
 return [
     'server' => [
-        'eurekaDefaultUrl' => 'http://discovery:8761/eureka/',
+        'eurekaDefaultUrl' => 'http://localhost:8761/eureka/',
         'hostName' => 'client-service',
         'appName' => 'client-service',
-        'ip' => 'discovery',
+        'ip' => 'localhost',
         'port' => ['8000', true],
-        'homePageUrl' => 'http://discovery:8000',
-        'statusPageUrl' => 'http://discovery:8000',
-        'healthCheckUrl' => 'http://discovery:8000/health'
+        'homePageUrl' => 'http://localhost:8000',
+        'statusPageUrl' => 'http://localhost:8000',
+        'healthCheckUrl' => 'http://localhost:8000/health'
     ]
 ];
